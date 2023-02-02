56223cb86e95a57ec5119123013d3fea15db7ecc
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-02-01 19:25:57 -0600

    finished ICalculator

--- calculator/src/project/ICalculator.java
+++ calculator/src/project/ICalculator.java
@@ -1,5 +1,9 @@
 package project;
 
 public interface ICalculator {
-
+	public float Add(float number1, float number2);
+	public float Substraction(float number1, float number2);
+	public float Multiply(float number1, float number2);
+	public float Divition(float number1, float number2);
+	public char getNextInput();
 }
